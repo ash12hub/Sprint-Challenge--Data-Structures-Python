d2a7236834fa04a1ebe862dfd864c73ed3f96e43
ash12hub <ash12hub@users.noreply.github.com>
2020-05-30 10:59:05 +0530

    Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.current_index = 0
+        self.capacity = capacity
+        self.buffer = []
 
     def append(self, item):
-        pass
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        else:
+            self.buffer[self.current_index] = item
+            self.current_index = (self.current_index + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.buffer
